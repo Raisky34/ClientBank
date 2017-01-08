d2cc461c76b0e244f6eb8e9e8e388b647c2affb2
anton <Raisky34@gmail.com>
2017-01-08 21:40:59 +0300

    Styles for home pages

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux'
 import Messages from './Messages';
 
+const styles = {
+  text: {
+    padding: '20% 20%',
+    textAlign: 'center',
+    fontSize: '25px'
+  }
+};
+
 class Home extends React.Component {
   render() {
     return (
-      <div className="container-fluid">
-        <p>Hello dear friend, glad to see you in our program.</p>
-        <p>We would like to tell you that you can not soveshat operation without a card,</p>
-        <p>which you want to add to your account.</p>
-        <p>The rest of the information you can get on Contact Form.</p>
-      </div>
+        <div style={styles.text}>
+          <p>Hello dear friend, glad to see you in our program.
+          We would like to tell you that you can not soveshat operation without a card,
+          which you want to add to your account.
+          The rest of the information you can get on Contact Form.</p>
+        </div>
     );
   }
 }
